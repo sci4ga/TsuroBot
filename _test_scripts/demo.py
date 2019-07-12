4191c3eb368ee7706af5feedbb3af7c89c3600fa
andrewtsai <andrew.tsai@turner.com>
2019-07-11 22:08:02 -0400

    test-wheels stuff only breaks on endpoint call

--- _test_scripts/demo.py
+++ _test_scripts/demo.py
@@ -7,60 +7,60 @@ bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 SPEED = 60
 bw_status = 0
 
-def run(action):
-    global SPEED, bw_status
-    debug = ''
-    # ============== Back wheels =============
-    if action == 'bwready':
-        bw.ready()
-        bw_status = 0
-    elif action == 'forward':
-        bw.speed = SPEED
-        bw.forward()
-        bw_status = 1
-        debug = "speed =", SPEED
-    elif action == 'backward':
-        bw.speed = SPEED
-        bw.backward()
-        bw_status = -1
-    elif action == 'stop':
-        bw.stop()
-        bw_status = 0
+# def run(action):
+#     global SPEED, bw_status
+#     debug = ''
+#     # ============== Back wheels =============
+#     if action == 'bwready':
+#         bw.ready()
+#         bw_status = 0
+#     elif action == 'forward':
+#         bw.speed = SPEED
+#         bw.forward()
+#         bw_status = 1
+#         debug = "speed =", SPEED
+#     elif action == 'backward':
+#         bw.speed = SPEED
+#         bw.backward()
+#         bw_status = -1
+#     elif action == 'stop':
+#         bw.stop()
+#         bw_status = 0
 
-    # ============== Front wheels =============
-    elif action == 'fwready':
-        fw.ready()
-    elif action == 'fwleft':
-        fw.turn_left()
-    elif action == 'fwright':
-        fw.turn_right()
-    elif action == 'fwstraight':
-        fw.turn_straight()
-    elif 'fwturn' in action:
-        print("turn %s" % action)
-        fw.turn(int(action.split(':')[1]))
+#     # ============== Front wheels =============
+#     elif action == 'fwready':
+#         fw.ready()
+#     elif action == 'fwleft':
+#         fw.turn_left()
+#     elif action == 'fwright':
+#         fw.turn_right()
+#     elif action == 'fwstraight':
+#         fw.turn_straight()
+#     elif 'fwturn' in action:
+#         print("turn %s" % action)
+#         fw.turn(int(action.split(':')[1]))
     
-    # ================ Camera =================
-    elif action == 'camready':
-        cam.ready()
-    elif action == "camleft":
-        cam.turn_left(40)
-    elif action == 'camright':
-        cam.turn_right(40)
-    elif action == 'camup':
-        cam.turn_up(20)
-    elif action == 'camdown':
-        cam.turn_down(20)
-    elif 'speed' in action:
-		speed = int(action.split(':')[1])
-		if speed < 0:
-			speed = 0
-		if speed > 100:
-			speed = 100
-		SPEED = speed
-		if bw_status != 0:
-			bw.speed = SPEED
-		debug = "speed =", speed
+#     # ================ Camera =================
+#     elif action == 'camready':
+#         cam.ready()
+#     elif action == "camleft":
+#         cam.turn_left(40)
+#     elif action == 'camright':
+#         cam.turn_right(40)
+#     elif action == 'camup':
+#         cam.turn_up(20)
+#     elif action == 'camdown':
+#         cam.turn_down(20)
+#     elif 'speed' in action:
+# 		speed = int(action.split(':')[1])
+# 		if speed < 0:
+# 			speed = 0
+# 		if speed > 100:
+# 			speed = 100
+# 		SPEED = speed
+# 		if bw_status != 0:
+# 			bw.speed = SPEED
+# 		debug = "speed =", speed
     
 bw.ready()
 bw_status = 0
--- api_tsuro.py
+++ api_tsuro.py
@@ -6,28 +6,30 @@ This is the tsuro module and supports all the ReST actions for the
 from flask import make_response
 # import native modules
 import logging
-import picar
-from picar import back_wheels, front_wheels
-
-picar.setup()
-db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
-fw = front_wheels.Front_Wheels(debug=False, db=db_file)
-bw = back_wheels.Back_Wheels(debug=False, db=db_file)
-bw.ready()
-fw.ready()
-
-SPEED = 60
-bw_status = 0
 
 def get_start():
     """
     This function responds to a request for /tsuro/start
     with a '200' upon successful startup
     """
-    global SPEED, bw_status
+
+    import picar
+    from picar import back_wheels, front_wheels
+
+    picar.setup()
+    db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
+    fw = front_wheels.Front_Wheels(debug=False, db=db_file)
+    bw = back_wheels.Back_Wheels(debug=False, db=db_file)
+    bw.ready()
+    fw.ready()
+
+    SPEED = 60
+    bw_status = 0
     bw.speed = SPEED
     bw.forward()
     bw_status = 1
+    import os
+    os.cmd('python3 ./tsuro/start.py')
     
     return make_response('Tsuro started: speed' + SPEED, 200)
 
@@ -36,8 +38,18 @@ def get_stop():
     This function responds to a request for /tsuro/stop
     with a '200' upon successful stop
     """
-    global SPEED, bw_status
+    import picar
+    from picar import back_wheels, front_wheels
 
+    picar.setup()
+    db_file = "/Users/andrewtsai/personal-projects/RobotTsuro/config"
+    fw = front_wheels.Front_Wheels(debug=False, db=db_file)
+    bw = back_wheels.Back_Wheels(debug=False, db=db_file)
+    bw.ready()
+    fw.ready()
+
+    SPEED = 60
+    bw_status = 0
     bw.stop()
     bw_status = 0
 
